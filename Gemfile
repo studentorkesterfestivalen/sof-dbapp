d96c0fa6e4444cde1ce727140e5bfec6f48f81d8
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-12-08 10:45:37 +0100

    Added dotenv to ease local development

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+# Enable use of .env file to specify environment variables for development
+gem 'dotenv-rails', :groups => [:development, :test]
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
 module SofDbapp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
