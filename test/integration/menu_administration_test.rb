cd3c9fbab6efaf8c40697ba9bb00bb3e13fd40e4
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-12-09 17:35:37 +0100

    Corrected incorrect use of exclamation mark

--- test/integration/menu_administration_test.rb
+++ test/integration/menu_administration_test.rb
@@ -6,18 +6,18 @@ class MenuAdministrationTest < ActionDispatch::IntegrationTest
   # of querying the API for this information?
 
   test 'add menu item' do
-    old_count = fetch_menu_items!.size
+    old_count = fetch_menu_items.size
 
     # Add new menu item
     post '/api/v1/menu', params: {item: {title: 'Test item'}}, as: :json
 
-    new_count = fetch_menu_items!.size
+    new_count = fetch_menu_items.size
 
     assert_equal old_count + 1, new_count, 'Menu item was not created'
   end
 
   test 'update menu item' do
-    items = fetch_menu_items!
+    items = fetch_menu_items
     item_to_update = items.first
 
     # Update title for menu item
@@ -28,20 +28,20 @@ class MenuAdministrationTest < ActionDispatch::IntegrationTest
   end
 
   test 'delete menu item' do
-    items = fetch_menu_items!
+    items = fetch_menu_items
     item_to_delete = items.first
     old_count = items.size
 
     delete api_v1_menu_url(item_to_delete['id'])
 
-    new_count = fetch_menu_items!.size
+    new_count = fetch_menu_items.size
 
     assert_equal old_count - 1, new_count, 'Menu item was not deleted'
   end
 
   private
 
-  def fetch_menu_items!
+  def fetch_menu_items
     get '/api/v1/menu'
     JSON.parse @response.body
   end
