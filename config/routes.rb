f8a7a6deaac95e84df4643e99c103a7c2d6bbaaf
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-01-17 00:24:11 +0100

    Added LetsEncrypt challenge endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Let’s encrypt
+  get '/.well-known/acme-challenge/:id' => 'lets_encrypt#challenge', as: :letsencrypt_challenge
 end
