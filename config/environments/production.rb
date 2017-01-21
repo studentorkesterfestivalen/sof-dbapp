7a17dd83c8e6be05070cb121be0d8a1b2e8bdedf
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-01-21 16:54:23 +0100

    Configure mailer url from environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Grab mailer URL from environment
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
