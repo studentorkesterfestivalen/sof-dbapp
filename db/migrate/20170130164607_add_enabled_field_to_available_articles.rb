56b6c5d5b3e5b22096e5cb576980991433442106
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-01-30 18:17:15 +0100

    Removed migration resulting in duplicate column

--- db/migrate/20170130164607_add_enabled_field_to_available_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddEnabledFieldToAvailableArticles < ActiveRecord::Migration[5.0]
-  def change
-    change_table :available_articles do |t|
-      t.boolean :enabled, default: true, null: false
-    end
-  end
-end
