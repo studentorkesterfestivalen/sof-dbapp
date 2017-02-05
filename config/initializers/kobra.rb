0a73759e50f2efd13f340f8d127e294476f5c405
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-02-05 15:59:32 +0100

    Added check for Kobra API key in server initialization

--- config/initializers/kobra.rb
+++ config/initializers/kobra.rb
@@ -1,2 +1,16 @@
 # Configuration for Kobra integration
 Rails.application.config.kobra_api_key = ENV.fetch('KOBRA_API_KEY') {''}
+
+# Ensure Kobra integration is working before starting the server
+if ENV['DISABLE_KOBRA_CHECK'].nil?
+  require 'kobra'
+
+  puts 'Verifying Kobra API key...'
+
+  kobra = Kobra::Client.new(api_key: Rails.configuration.kobra_api_key)
+  kobra.get_student(id: 'patsl736', union: true)
+
+  puts 'Kobra check passed'
+else
+  puts 'WARNING: Kobra check disabled'
+end
