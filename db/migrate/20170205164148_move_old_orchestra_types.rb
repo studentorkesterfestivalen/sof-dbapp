e6dfe09419e8b74d334a48a8c0893c049ba7cdc1
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-02-06 23:01:39 +0100

    Rewrite orchestra_type migration to execute on PostgreSQL databases. Removed broken sqlite3 migration code

--- db/migrate/20170205164148_move_old_orchestra_types.rb
+++ db/migrate/20170205164148_move_old_orchestra_types.rb
@@ -1,5 +1,9 @@
 class MoveOldOrchestraTypes < ActiveRecord::Migration[5.0]
   def self.up
-    Orchestra.update_all('orchestra_type=ballet')
+    if Rails.env.production?
+      execute 'UPDATE orchestras SET orchestra_type = CASE ballet WHEN true THEN 1 ELSE 0 END'
+    else
+      # Migration disabled for non-production databases since command does not work for sqlite3
+    end
   end
 end
