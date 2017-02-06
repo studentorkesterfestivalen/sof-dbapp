5a867f049498445032f29fe2e7c61e866436ffba
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-02-06 23:33:14 +0100

    Modified productiontest.sh to execute with new Kobra integration and modified database test

--- test/development/productiontest.sh
+++ test/development/productiontest.sh
@@ -1,6 +1,11 @@
+read -p "Enter Kobra API key: " kobra
+
+export LC_CTYPE=C
 export DATABASE_URL="postgresql://vagrant@localhost/vagrant"
 export RAILS_ENV=production
 export SECRET_KEY_BASE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+export KOBRA_API_KEY=$kobra
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
-rake db:reset db:migrate db:seed
+rake db:drop db:create db:migrate db:seed
 rails s -p 3001
