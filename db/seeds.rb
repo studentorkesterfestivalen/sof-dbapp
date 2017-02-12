ed7f700fe110181397814e8278ce8d85a88284f2
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-02-12 13:29:51 +0100

    Added case cortege management page to menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,8 @@ test_menu_items = [
     ['Administration', '#', true, 'admin', 0, false, [
         ['Hantera användare', '/manage/users', true, '', Permission::LIST_USERS, true, []],
         ['Hantera orkestrar', '/manage/orchestras', true, '', Permission::LIST_ORCHESTRA_SIGNUPS, true, []],
-        ['Hantera kårteger', '/manage/corteges', true, '', Permission::LIST_CORTEGE_APPLICATIONS, true, []]
+        ['Hantera kårteger', '/manage/corteges', true, '', Permission::LIST_CORTEGE_APPLICATIONS, true, []],
+        ['Hantera casekårteger', '/manage/case_corteges', true, '', Permission::LIST_CORTEGE_APPLICATIONS, true, []]
     ]]
 ]
 
