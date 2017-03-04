c1a002038ceb2e3eb5a11f21e3ffd4395482db24
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-03-04 01:18:38 +0100

    Validate existance of product for cart items

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,3 +1,13 @@
 class CartItem < ApplicationRecord
   belongs_to :product
+
+  validate :has_existing_product
+
+  private
+
+  def has_existing_product
+    if product.nil?
+      errors.add(:product, 'must exist')
+    end
+  end
 end
