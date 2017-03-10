0dcf8628c3b3e99f6114dde30ca49203ccc8932b
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-03-10 11:35:00 +0100

    Use column separators with better Excel compatibility

--- app/export/csv_export.rb
+++ app/export/csv_export.rb
@@ -2,7 +2,7 @@ class CSVExport
   class << self
     def render_csv(data, format)
       columns = format.column_names
-      CSV.generate do |csv|
+      CSV.generate(:col_sep => ';') do |csv|
         csv << columns.values
         data.each do |item|
           values = columns.keys.map { |col| format.data_for(item, col) }
