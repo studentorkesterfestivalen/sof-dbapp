dd0d2e25c3d8d124d71f12f9340b158df06d90e8
Lukas Lindqvist <mail@lukaslindqvist.se>
2017-03-27 09:28:49 +0200

    Removed link to casecortege application page

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ test_menu_items = [
     ['Kårtege', '#', true, 'cortege', 0, true, [
         ['Om Kårtegen', '/cortege', true, '', 0, true, []],
         ['Om Casekårtege', '/case_cortege', true, '', 0, true, []],
-        #['Kårtegeanmälan', '/cortege/interest', true, '', 0, true, []],
-        ['Casekårtegeanmälan', '/case_cortege/new', true, '', 0, true, []],
+        ['Kårtegeanmälan', '/cortege/interest', false, '', 0, true, []],
+        ['Casekårtegeanmälan', '/case_cortege/new', false, '', 0, true, []],
     ]],
     ['Jobba på SOF', '#', true, 'funkis', 0, true, [
         ['Förmåner', '/funkis', true, '', 0, true, []],
@@ -518,14 +518,15 @@ end
 # Still enabled on development and test so that its easy to test stuff.
 
 if Rails.env.development? or Rails.env.test?
-    FunkisCategory.delete_all
-    FunkisShift.delete_all
-    funkis_categories.each { |c| create_funkis_category *c }
+  FunkisApplication.delete_all
+  FunkisCategory.delete_all
+  FunkisShift.delete_all
+  funkis_categories.each { |c| create_funkis_category *c }
 
-    ActiveFunkisShiftLimit.delete_all
-    ActiveFunkisShiftLimit.create!(
-        active_limit: 0
-    )
+  ActiveFunkisShiftLimit.delete_all
+  ActiveFunkisShiftLimit.create!(
+      active_limit: 0
+  )
 end
 
 
