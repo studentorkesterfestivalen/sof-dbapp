77ebdfa618c4e0d0fb3ceedd0e9e70cf14e3a1a0
Lukas Lindqvist <mail@lukaslindqvist.se>
2017-04-07 18:25:53 +0200

    added check that parameter exists

--- lib/tasks/user.rake.rb
+++ lib/tasks/user.rake.rb
@@ -1,8 +1,12 @@
 namespace :user do
   task :resend_confirmation, [:uid] => :environment do |t, args|
-    users = User.where('confirmation_token IS NOT NULL').and(User.where(:uid => args[:uid]))
-    users.each do |user|
-      user.send_confirmation_instructions
+    if args[:uid]
+      users = User.where('confirmation_token IS NOT NULL').and(User.where(:uid => args[:uid]))
+      users.each do |user|
+        user.send_confirmation_instructions
+      end
+    else
+      puts 'Missing UID parameter.'
     end
   end
 end
