0781b89c2a09a1cb5a48c8333084c681a84b63c7
Lukas Lindqvist <mail@lukaslindqvist.se>
2017-04-24 13:14:39 +0200

    Update paid flag in progress

--- app/controllers/api/v1/cortege_controller.rb
+++ app/controllers/api/v1/cortege_controller.rb
@@ -6,6 +6,8 @@ class API::V1::CortegeController < ApplicationController
   def index
     require_admin_permission AdminPermission::LIST_CORTEGE_APPLICATIONS
 
+    update_paid_flag
+
     render :json => Cortege.all, include: [:user]
   end
 
@@ -63,6 +65,13 @@ class API::V1::CortegeController < ApplicationController
   end
 
   private
+
+  def update_paid_flag
+    corteges = Cortege.all
+    corteges.each do |cortege|
+
+    end
+  end
 
   def item_params
     params.require(:item).permit(
