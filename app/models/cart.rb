e996fcfe61d6dacf0db7562f1b9e52f89d94c762
Lukas Lindqvist <mail@lukaslindqvist.se>
2017-04-24 21:56:35 +0200

    Kårrebate only added when user is actually in lintek

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,27 +10,31 @@ class Cart < ApplicationRecord
   }
 
   def rebate
-    items = cart_items
-    given_lintek_rebates = Set.new
+    if user.is_lintek_member
+      items = cart_items
+      given_lintek_rebates = Set.new
 
-    items.each do |item|
-      if item.product.base_product.name == 'Dagsbiljett' or item.product.base_product.name == 'Helhelgsbiljett'
-        if item.product.current_count(user, []) == 0
-          case item.product.kind
-            when 'Torsdag'
-              given_lintek_rebates << :thursday
-            when 'Fredag'
-              given_lintek_rebates << :friday
-            when 'Lördag'
-              given_lintek_rebates << :saturday
-            else
-              given_lintek_rebates << :weekend
+      items.each do |item|
+        if item.product.base_product.name == 'Dagsbiljett' or item.product.base_product.name == 'Helhelgsbiljett'
+          if item.product.current_count(user, [])
+            case item.product.kind
+              when 'Torsdag'
+                given_lintek_rebates << :thursday
+              when 'Fredag'
+                given_lintek_rebates << :friday
+              when 'Lördag'
+                given_lintek_rebates << :saturday
+              else
+                given_lintek_rebates << :weekend
+            end
           end
         end
       end
-    end
 
-    given_lintek_rebates.sum { |x| LINTEK_REBATES[x] }
+      given_lintek_rebates.sum { |x| LINTEK_REBATES[x] }
+    else
+      0
+    end
   end
 
   def funkis_rebate
