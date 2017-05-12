fe348f379fbc4595ea9d89e2d5f2cb312212ca15
Lukas Lindqvist <mail@lukaslindqvist.se>
2017-05-12 21:52:46 +0200

    now sends receipt email on products being bought

---
+++ app/mailers/receipt_mailer.rb
@@ -0,0 +1,12 @@
+class ReceiptMailer < ApplicationMailer
+  default from: 'no-reply@sof17.se'
+
+  def order_receipt(order)
+    @user = order.user
+    @order = order
+    @total = order.order_items.sum { |x| x.cost } - order.rebate - order.funkis_rebate
+
+
+    mail(to: @user.email, subject: 'SOF17: Kvitto för order')
+  end
+end
