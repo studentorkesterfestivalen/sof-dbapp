e4eefc3743eb9d615118f42c109779ff7aab73b6
Mathias Berggren <mathiasberggren1@gmail.com>
2019-02-09 11:35:03 +0100

    Enabling registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :omniauthable
-  #include DeviseTokenAuth::Concerns::User
+  include DeviseTokenAuth::Concerns::User
 
-  
+
 
   include UserPermissionConcern
 
