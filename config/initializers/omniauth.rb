dead8fc503244cff0e765947e379c00592800636
NaabZer <anton.gefvert@gmail.com>
2019-02-11 12:34:35 +0100

    Fix cas auth url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ require 'ext/cas'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cas,
-           host:      'login.liu.se',
-           login_url: '/cas/login',
-           service_validate_url: '/serviceValidate'
+           host:      'login.it.liu.se',
+           login_url: '/idp/profile/cas/login',
+           service_validate_url: '/idp/profile/cas/serviceValidate'
 end
