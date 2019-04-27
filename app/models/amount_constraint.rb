53e0d687fd33cfcd9d88b42cffa3cede20348e07
NaabZer <anton.gefvert@gmail.com>
2019-04-27 18:39:35 +0200

    Add amount_left function

--- app/models/amount_constraint.rb
+++ app/models/amount_constraint.rb
@@ -1,3 +1,12 @@
 class AmountConstraint < ApplicationRecord
   has_and_belongs_to_many :products
+
+  def amount_left
+    amount_left = amount
+    products.each do |prod|
+      amt -= prod.amount_bought
+    end
+
+    amount_left
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -38,8 +38,24 @@ class Product < ApplicationRecord
     (user.purchased_items + additional_items).count { |x| x.product.id == id }
   end
 
+  def amount_left
+    smallest_amount = -1
+    if max_num_available > 0
+      smallest_amount = max_num_available - amount_bought
+    end
+
+    amount_constraints.each do |constraint|
+      amt = constraint.amount_left
+      if(smallest_amount == -1 || amt < smallest_amount)
+        smallest_amount = amt
+      end
+    end
+
+    smallest_amount
+  end
+
   def amount_bought
-    OrderItem.where(product_id: id).count
+    OrderItem.where(product_id: id).sum{ |prod| prod.amount } 
   end
 
 end
