53d85865002cf626625321a480921a1a7c1d97a2
Mathias Berggren <mathiasberggren1@gmail.com>
2019-04-27 15:32:04 +0200

    Save receipt_url from Stripe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190425103754) do
+ActiveRecord::Schema.define(version: 20190427124200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -284,6 +284,7 @@ ActiveRecord::Schema.define(version: 20190425103754) do
     t.integer  "rebate",         default: 0,     null: false
     t.integer  "funkis_rebate",  default: 0,     null: false
     t.boolean  "receipt_sent",   default: false, null: false
+    t.string   "receipt_url"
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
 
