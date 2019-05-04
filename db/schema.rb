c482fd0310a5d71687bd6723804b932bd460dac8
Mathias Berggren <mathiasberggren1@gmail.com>
2019-05-04 16:59:39 +0200

    Schema file

--- db/schema.rb
+++ db/schema.rb
@@ -124,26 +124,11 @@ ActiveRecord::Schema.define(version: 20190504072212) do
     t.index ["user_id"], name: "index_corteges_on_user_id", using: :btree
   end
 
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
-  end
-
   create_table "discount_codes", force: :cascade do |t|
     t.integer  "discount"
     t.integer  "uses"
-    t.integer  "product_id"
     t.string   "code",       null: false
+    t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_discount_codes_on_product_id", using: :btree
